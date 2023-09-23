6a89c4897684d878bb24919836d9c301de1825df
dohyunre702 <dohyunre702@gmail.com>
2023-09-23 16:10:28 +0900

    매개변수의 다형성 (parametric polymorphism) + 참조변수 배열 (reference array)

--- src/main/java/javaJungsuk/OOP/PolyArgumentTest.java
+++ src/main/java/javaJungsuk/OOP/PolyArgumentTest.java
@@ -5,18 +5,24 @@ public class PolyArgumentTest {
         Buyer b = new Buyer();
 
         b.buy(new Television());
-        System.out.println();
         b.buy(new Computer());
+        b.buy(new Audio());
+        b.summary();
     }
 }
 
 class Product {
     int price;
-    int bonuspoint;
+    int bonusPoint;
 
     Product (int price) {
         this.price = price;
-        bonuspoint = (int)(price/10.0);
+        bonusPoint = (int)(price/10.0);
+    }
+
+    Product() {
+        price = 0;
+        bonusPoint = 0;
     }
 }
 
@@ -36,9 +42,17 @@ class Computer extends Product {
     public String toString() {return "Computer";}
 }
 
+class Audio extends Product {
+    Audio() {
+        super(50);
+    }
+    public String toString() {return "Audio";}
+}
 class Buyer {
     int money = 1000;
     int bonusPoint = 0;
+    Product[] item = new Product[10]; //구입한 제품을 저장하는 참조변수 배열
+    int i = 0;
 
     void buy(Product p) {
         if(money < p.price) {
@@ -46,11 +60,26 @@ class Buyer {
             return;
         }
         money -= p.price;
-        bonusPoint += p.bonuspoint;
+        bonusPoint += p.bonusPoint;
+        item[i++] = p; //제품을 Product[] item에 저장
         System.out.println(p + "을/를 구입하셨습니다.");
 
         System.out.println("현재 남은 돈은 " + money + "만원입니다.");
         System.out.println("현재 보너스 점수는 " + bonusPoint + "점입니다.");
+        System.out.println();
+    }
+
+    void summary() {
+        int sum = 0;
+        String itemList = "";
+
+        for(int i = 0 ; i < item.length; i++) {
+            if(item[i] == null) break;
+            sum += item[i].price;
+            itemList += item[i] + ", ";
+        }
+        System.out.println("구입하신 물품의 총금액은 " + sum + "만원입니다.");
+        System.out.println("구입하신 제품은 " + itemList + "입니다.");
     }
 
 }
